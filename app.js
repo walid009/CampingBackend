ff32b04d397181a830b0a8437072756ef61fac5f
chekir walid <walid.chekir@esprit.tn>
2021-10-31 14:52:14 +0100

    creation des tables et quelques elements

--- app.js
+++ app.js
@@ -9,10 +9,239 @@ const client = new MongoClient(url);
 
 client.connect(function(err){
     assert.equal(null,err)
-    console.log("connexion etablie");
     const db = client.db(dbName);
-    client.close();
+    console.log("connexion etablie");
+    insertDocumentsUtilisateur(db, function(){
+    })
+    insertDocumentsEvenement(db,function(){
+    })
+    insertDocumentsFeedBack(db,function(){
+    })
+    insertDocumentsFavorite(db,function(){
+      client.close();
+    })
 })
+
+const insertDocumentsUtilisateur = function(db, callback) {
+  const collection = db.collection('utilisateur');
+  collection.insertMany([
+    {
+      nom: "chek",
+      prenom: "walid",
+      email: "walid@gmail.com",
+      password: "123",
+      telephone: 5555555,
+      role: "campeur",
+      evenements: [
+        {
+          titre: "camping aindrahem",
+          description: "1 nuit dans aindrahem",
+          position:{
+            Longitude: 123.0,
+            Latitude: 34.0
+          },
+          createur:{
+            nom: "org",
+            prenom: "org",
+            email: "org@gmail.com",
+            password: "123",
+            telephone: 66666666,
+            role: "campeur"
+          },
+          participants:[
+            {
+              nom: "chek",
+              prenom: "walid",
+              email: "walid@gmail.com",
+              password: "123",
+              telephone: 5555555,
+              role: "campeur"
+            }
+          ],
+        }
+      ]
+    },
+    {
+      nom: "org",
+      prenom: "org",
+      email: "org@gmail.com",
+      password: "123",
+      telephone: 66666666,
+      role: "organisateur",
+      evenements: [
+        {
+          titre: "camping aindrahem",
+          description: "1 nuit dans aindrahem",
+          position:{
+            Longitude: 123.0,
+            Latitude: 34.0
+          },
+          createur:{
+            nom: "org",
+            prenom: "org",
+            email: "org@gmail.com",
+            password: "123",
+            telephone: 66666666,
+            role: "organisateur"
+          },
+          participants:[
+            {
+              nom: "chek",
+              prenom: "walid",
+              email: "walid@gmail.com",
+              password: "123",
+              telephone: 5555555,
+              role: "campeur"
+            }
+          ],
+        }
+      ]
+    },
+    {
+      nom: "adm",
+      prenom: "adm",
+      email: "adm@gmail.com",
+      password: "123",
+      telephone: 7777777,
+      role: "admin"
+    }
+  ],function(err, result){
+    assert.equal(err,null);
+    //assert.equal(3, result.result.n);
+    //assert.equal(3, result.ops.length);
+    console.log("insertion de 3 utilisateurs");
+    callback(result);
+  });
+}
+
+const insertDocumentsEvenement = function(db, callback) {
+  const collection = db.collection('evenement');
+  collection.insertOne(
+    {
+      titre: "camping aindrahem",
+      description: "1 nuit dans aindrahem",
+      position:{
+        Longitude: 123.0,
+        Latitude: 34.0
+      },
+      createur:{
+        nom: "org",
+        prenom: "org",
+        email: "org@gmail.com",
+        password: "123",
+        telephone: 66666666,
+        role: "organisateur"
+      },
+      participants:[
+        {
+          nom: "chek",
+          prenom: "walid",
+          email: "walid@gmail.com",
+          password: "123",
+          telephone: 5555555,
+          role: "campeur"
+        }
+      ]
+    },function(err, result){
+    assert.equal(err,null);
+    //assert.equal(3, result.result.n);
+    //assert.equal(3, result.ops.length);
+    console.log("insertion d un evenement");
+    callback(result);
+  });
+}
+const insertDocumentsFeedBack = function(db, callback) {
+  const collection = db.collection('feedback');
+  collection.insertOne(
+    {
+      evenement:{
+        titre: "camping aindrahem",
+        description: "1 nuit dans aindrahem",
+        position:{
+          Longitude: 123.0,
+          Latitude: 34.0
+        },
+        createur:{
+          nom: "org",
+          prenom: "org",
+          email: "org@gmail.com",
+          password: "123",
+          telephone: 66666666,
+          role: "organisateur"
+        },
+        participants:[
+          {
+            nom: "chek",
+            prenom: "walid",
+            email: "walid@gmail.com",
+            password: "123",
+            telephone: 5555555,
+            role: "campeur"
+          }
+        ],
+      },
+      campeurs:[
+        {
+          nom: "chek",
+          prenom: "walid",
+          email: "walid@gmail.com",
+          password: "123",
+          telephone: 5555555,
+          role: "campeur",
+          commentaire: "j apprecie cette evenement",
+          evaluation: 3
+        }
+      ]
+    },function(err, result){
+    assert.equal(err,null);
+    //assert.equal(3, result.result.n);
+    //assert.equal(3, result.ops.length);
+    console.log("insertion d un feedback");
+    callback(result);
+  });
+}
+
+const insertDocumentsFavorite = function(db, callback) {
+  const collection = db.collection('favorite');
+  collection.insertOne(
+    {
+      evenements:[
+        {
+          titre: "camping aindrahem",
+          description: "1 nuit dans aindrahem",
+          position:{
+            Longitude: 123.0,
+            Latitude: 34.0
+          },
+          createur:{
+            nom: "org",
+            prenom: "org",
+            email: "org@gmail.com",
+            password: "123",
+            telephone: 66666666,
+            role: "organisateur"
+          },
+          participants:[
+            {
+              nom: "chek",
+              prenom: "walid",
+              email: "walid@gmail.com",
+              password: "123",
+              telephone: 5555555,
+              role: "campeur"
+            }
+          ],
+        }
+      ],
+      partager: false
+    },function(err, result){
+    assert.equal(err,null);
+    //assert.equal(3, result.result.n);
+    //assert.equal(3, result.ops.length);
+    console.log("favorisation d un evenement");
+    callback(result);
+  });
+}
 
 /*const { MongoClient } = require("mongodb");
 
